a54105398b101eb72a0c7e6d5c540e0ce2e37aab
DATBOI <alexsimons9999@gmail.com>
2019-04-07 16:25:21 -0500

    Very nice, I can logout now :D

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Button, Platform, StyleSheet, Text, View} from 'react-native';
 import {authorize} from "react-native-app-auth";
+import axios from 'axios';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,10 +10,12 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-const issuer = 'http://192.168.43.44:8080/auth/realms/master';
+const issuer = 'http://172.17.0.1:8080/auth/realms/master';
+const revocationEndpoint = `${issuer}/protocol/openid-connect/logout`;
+const clientId = 'sogos-app';
 const configuration = {
   issuer,
-  clientId: 'sogos-app',
+  clientId,
   redirectUrl: 'io.acari.sogos:/engage',
   scopes: ['openid', 'profile', 'email', 'offline_access'],
   additionalParameters: {
@@ -21,22 +24,46 @@ const configuration = {
   serviceConfiguration: {
     authorizationEndpoint: `${issuer}/protocol/openid-connect/auth`,
     tokenEndpoint: `${issuer}/protocol/openid-connect/token`,
-    revocationEndpoint: `${issuer}/protocol/openid-connect/logout`,
+    revocationEndpoint,
   },
   dangerouslyAllowInsecureHttpRequests: true,//todo: remove dis
+};
+
+const revokeToken = async (accessToken: String, refreshToken: String) => {
+  return axios.post(revocationEndpoint,
+    `client_id=${clientId}&refresh_token=${refreshToken}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 };
 
 type Props = {};
 export default class App extends Component<Props> {
 
   state = {
-    loggedIn : false,
+    loggedIn: false,
+    refreshToken: '',
+    accessToken: '',
+  };
+
+  logout = async () => {
+    try {
+      const {refreshToken, accessToken} = this.state;
+      // todo: refresh access token before logging out
+      await revokeToken(accessToken, refreshToken);
+      this.setState({loggedIn: false})
+    } catch (e) {
+      console.warn('Shit broke yo', e.message);
+    }
   };
 
   login = async () => {
     try {
-      const appAuth = authorize(configuration);
-      this.setState({loggedIn: true, appAuth})
+      const {refreshToken, accessToken} = await authorize(configuration);
+      this.setState({loggedIn: true, refreshToken, accessToken})
     } catch (error) {
       console.warn(`Shit broke yo`, error)
     }
@@ -50,6 +77,7 @@ export default class App extends Component<Props> {
           loggedIn ?
             <View>
               <Text>You are logged in, Hurray!!</Text>
+              <Button title={'Logout'} onPress={this.logout}/>
             </View> :
             <View>
               <Text style={styles.welcome}>Welcome to React Native!</Text>
