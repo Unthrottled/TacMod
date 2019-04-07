d36ad56883ebec63d37c6a2644f0fe75fec0fe90
DATBOI <alexsimons9999@gmail.com>
2019-04-07 13:33:55 -0500

    I can log in now

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import {authorize} from "react-native-app-auth";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +9,55 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const issuer = 'http://192.168.43.44:8080/auth/realms/master';
+const configuration = {
+  issuer,
+  clientId: 'sogos-app',
+  redirectUrl: 'io.acari.sogos:/engage',
+  scopes: ['openid', 'profile', 'email', 'offline_access'],
+  additionalParameters: {
+    prompt: 'login',
+  },
+  serviceConfiguration: {
+    authorizationEndpoint: `${issuer}/protocol/openid-connect/auth`,
+    tokenEndpoint: `${issuer}/protocol/openid-connect/token`,
+    revocationEndpoint: `${issuer}/protocol/openid-connect/logout`,
+  },
+  dangerouslyAllowInsecureHttpRequests: true,//todo: remove dis
+};
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    loggedIn : false,
+  };
+
+  login = async () => {
+    try {
+      const appAuth = authorize(configuration);
+      this.setState({loggedIn: true, appAuth})
+    } catch (error) {
+      console.warn(`Shit broke yo`, error)
+    }
+  };
+
   render() {
+    const {loggedIn} = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {
+          loggedIn ?
+            <View>
+              <Text>You are logged in, Hurray!!</Text>
+            </View> :
+            <View>
+              <Text style={styles.welcome}>Welcome to React Native!</Text>
+              <Text style={styles.instructions}>To get started, edit App.js</Text>
+              <Text style={styles.instructions}>{instructions}</Text>
+              <Button title={'Login Yo'} onPress={this.login}>Login Yo</Button>
+            </View>
+        }
       </View>
     );
   }
