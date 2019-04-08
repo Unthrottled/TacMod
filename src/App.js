21fb7a2fc0a0c9534dd103c8af777ad6bbb06d27
DATBOI <alexsimons9999@gmail.com>
2019-04-08 05:24:58 -0500

    Moar stoar persist

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import {applyMiddleware, createStore} from "redux";
 import rootReducer from 'reducers'
 import RootView from "./RootView";
 import thunk from "redux-thunk";
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import {PersistGate} from "redux-persist/lib/integration/react";
 
 const middleware = [thunk];
 
-if(__DEV__){
+if (__DEV__) {
   require('./config/ReactoTronConfig');
 }
 
-const store = createStore(rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['security'],
+};
+
+const store = createStore(
+  persistReducer(persistConfig, rootReducer),
   applyMiddleware(...middleware));
+const persistor = persistStore(store);
 
 export default () =>
   <Provider store={store}>
-    <RootView/>
+    {/*todo: loading indicator*/}
+    <PersistGate persistor={persistor}>
+      <RootView/>
+    </PersistGate>
   </Provider>
