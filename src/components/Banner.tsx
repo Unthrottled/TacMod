5c49d3d06b7bf092cd3cfd1ec7a484125a8f1b35
DATBOI <alexsimons9999@gmail.com>
2019-12-21 18:03:02 -0600

    More Banner Component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,58 @@
+import React, {FC} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import ReachIcon from '../images/ReachIcon';
+import {Caption, Paragraph} from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  parent: {
+    height: '100%',
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#F5FCFF',
+  },
+  banner: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    backgroundColor: '#F5FCFF',
+    borderRadius: 10,
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    margin: 10,
+    maxHeight: 400,
+  },
+  secondary: {
+    color: 'slategrey',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#e49d2c',
+    color: '#585858',
+    textDecorationColor: '#585858',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+});
+
+const Banner: FC = ({children}) => (
+  <View style={styles.banner}>
+    <View style={styles.container}>
+      <Text style={{fontSize: 35}}>SOGoS</Text>
+      <Caption style={styles.secondary}>
+        Strategic Orchestration and Governance System
+      </Caption>
+      <Paragraph style={styles.secondary}>
+        Find and reach your maximum potential! Push yourself to the limits of
+        your ability. Knowing you rest easy when you really need to.
+      </Paragraph>
+      <ReachIcon />
+      {children}
+    </View>
+  </View>
+);
+
+export default Banner;
--- src/views/LoggedOut.tsx
+++ src/views/LoggedOut.tsx
@@ -2,42 +2,11 @@ import React, {FC, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import {createApplicationInitializedEvent} from '../events/ApplicationLifecycleEvents';
 import {createRequestLogonEvent} from '../events/SecurityEvents';
-import {StyleSheet, Text, View} from 'react-native';
-import {
-  Button,
-  Caption,
-  Headline,
-  Paragraph,
-  Subheading,
-} from 'react-native-paper';
-import ReachIcon from '../images/ReachIcon';
+import {StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
+import Banner from '../components/Banner';
 
 const styles = StyleSheet.create({
-  parent: {
-    height: '100%',
-  },
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 15,
-    backgroundColor: '#F5FCFF',
-  },
-  banner: {
-    marginTop: 'auto',
-    marginBottom: 'auto',
-    backgroundColor: '#F5FCFF',
-    borderRadius: 10,
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-    textAlign: 'center',
-    margin: 10,
-    maxHeight: 400,
-  },
-  secondary: {
-    color: 'slategrey',
-    textAlign: 'center',
-  },
   button: {
     backgroundColor: '#e49d2c',
     color: '#585858',
@@ -62,22 +31,13 @@ const LoggedOut: FC = () => {
   }, [dispetch]);
 
   return (
-    <View style={styles.banner}>
-      <View style={styles.container}>
-        <Text style={{fontSize: 35}}>SOGoS</Text>
-        <Caption style={styles.secondary}>
-          Strategic Orchestration and Governance System
-        </Caption>
-        <Paragraph style={styles.secondary}>
-          Find and reach your maximum potential! Push yourself to the limits of
-          your ability. Knowing you rest easy when you really need to.
-        </Paragraph>
-        <ReachIcon />
+    <>
+      <Banner>
         <Button color={'#585858'} style={styles.button} onPress={login}>
           Start Using SOGoS!
         </Button>
-      </View>
-    </View>
+      </Banner>
+    </>
   );
 };
 
