5546bba6fa33176ce252adc2e0fde8c280b447b7
DATBOI <alexsimons9999@gmail.com>
2019-12-27 07:48:05 -0600

    More able to manually sync

---
+++ src/components/ManualSync.tsx
@@ -0,0 +1,34 @@
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {GlobalState, selectNetworkState, selectUserState} from '../reducers';
+import {useDispatch, useSelector} from 'react-redux';
+import {createRequestedSyncEvent} from '../events/UserEvents';
+
+const mapStateToProps = (state: GlobalState) => {
+  const {isOnline} = selectNetworkState(state);
+  const {
+    miscellaneous: {hasItemsCached},
+  } = selectUserState(state);
+  return {
+    isOnline,
+    hasItemsCached,
+  };
+};
+
+const ManualSync = () => {
+  const {isOnline, hasItemsCached} = useSelector(mapStateToProps);
+
+  const shouldDisplay = isOnline && hasItemsCached;
+
+  const dispetch = useDispatch();
+
+  return shouldDisplay ? (
+    <TouchableOpacity onPress={() => dispetch(createRequestedSyncEvent())}>
+      <MaterialIcon name={'sync'} size={25} />
+    </TouchableOpacity>
+  ) : null;
+};
+
+export default ManualSync;
--- src/components/MenuAppBar.tsx
+++ src/components/MenuAppBar.tsx
@@ -2,10 +2,12 @@ import React, {FC} from 'react';
 import {Appbar} from 'react-native-paper';
 import MenuOptions from './MenuOptions';
 import {View} from 'react-native';
+import ManualSync from './ManualSync';
 
 const MenuAppBar: FC = () => {
   return (
     <Appbar.Header>
+      <ManualSync />
       <View style={{flex: 1}} />
       <MenuOptions />
     </Appbar.Header>
