92ebd1abcf7bc0d66013b8fcaad52af7114e90f3
DATBOI <alexsimons9999@gmail.com>
2020-01-15 05:56:23 -0600

    More visible loading indicator

--- src/views/AuthLoading.tsx
+++ src/views/AuthLoading.tsx
@@ -1,10 +1,17 @@
 import React, {useEffect} from 'react';
-import {StatusBar, View} from 'react-native';
+import {StatusBar, StyleSheet, View} from 'react-native';
 import {ActivityIndicator} from 'react-native-paper';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectSecurityState} from '../reducers';
 import {useNavigation} from 'react-navigation-hooks';
 import {createApplicationInitializedEvent} from '../events/ApplicationLifecycleEvents';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+});
 
 const AuthLoading = () => {
   const {isLoggedIn, isInitialized} = useSelector(selectSecurityState);
@@ -17,9 +24,8 @@ const AuthLoading = () => {
     navigate(isLoggedIn ? 'LoggedIn' : 'Login');
   }, [dispetch, isInitialized, isLoggedIn, navigate]);
   return (
-    <View>
-      <ActivityIndicator />
-      <StatusBar barStyle={'default'} />
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} />
     </View>
   );
 };
