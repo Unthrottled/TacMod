6e31748e460e061543829349d49b2bcdba7a990a
DATBOI <alexsimons9999@gmail.com>
2020-01-15 04:48:38 -0600

    Renamed ios stuff

--- ios/TacModTests/TacModTests.m
+++ ios/TacModTests/TacModTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SOGoSTests : XCTestCase
+@interface TacModTests : XCTestCase
 
 @end
 
-@implementation SOGoSTests
+@implementation TacModTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
