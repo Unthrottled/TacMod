ce62528880c28ab76716d7852193ccd8456c5435
DATBOI <alexsimons9999@gmail.com>
2020-02-11 04:33:19 -0600

    Wired in the alarm stuff

--- android/app/src/main/java/io/unthrottled/sogos/tacmod/MainApplication.kt
+++ android/app/src/main/java/io/unthrottled/sogos/tacmod/MainApplication.kt
@@ -1,7 +1,11 @@
 package io.unthrottled.sogos.tacmod
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
@@ -37,6 +41,24 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this,  /* native exopackage */false)
     initializeFlipper(this) // Remove this line if you don't want Flipper enabled
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      initializeAlarm()
+    }
+  }
+
+  @RequiresApi(Build.VERSION_CODES.O)
+  fun initializeAlarm() {
+    val notificationChannel = NotificationChannel(
+        "TacModNotifications",
+        "TacMod",
+        NotificationManager.IMPORTANCE_DEFAULT
+    )
+    notificationChannel.enableVibration(true)
+    notificationChannel.vibrationPattern = longArrayOf(100L,200L,300L);
+
+    this.getSystemService(NotificationManager::class.java)
+        ?.createNotificationChannel(notificationChannel)
   }
 
   companion object {
