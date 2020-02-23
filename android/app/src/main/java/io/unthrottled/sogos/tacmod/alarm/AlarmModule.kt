27d343d0639edf10af1527c4046a1a060ef410ae
DATBOI <alexsimons9999@gmail.com>
2020-02-23 09:52:57 -0600

    Able to schedule a notification in the future

--- android/app/src/main/java/io/unthrottled/sogos/tacmod/alarm/AlarmModule.kt
+++ android/app/src/main/java/io/unthrottled/sogos/tacmod/alarm/AlarmModule.kt
@@ -1,17 +1,19 @@
 package io.unthrottled.sogos.tacmod.alarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.SystemClock
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
-import okhttp3.OkHttpClient
+import java.time.Instant
+import java.util.*
 
-class AlarmModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class AlarmModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName(): String = "Alarm"
-
-    companion object {
-        private val client = OkHttpClient()
-    }
 
     @ReactMethod
     fun stopAllAlarms() {
@@ -21,5 +23,22 @@ class AlarmModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaM
     @ReactMethod
     fun setAlarm(alarmParameters: ReadableMap) {
         println("finna set an alarm to this $alarmParameters")
+        val alarmIntent = Intent(reactContext.applicationContext, AlarmReceiver::class.java)
+        val pendingAlarmIntent = PendingIntent.getBroadcast(
+                reactContext.applicationContext,
+                Instant.now().epochSecond.toInt(),
+                alarmIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        )
+
+        val alarmManager = reactContext.applicationContext.getSystemService(
+                Context.ALARM_SERVICE
+        ) as AlarmManager
+
+        alarmManager.set(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 5000,
+                pendingAlarmIntent
+        )
     }
 }
---
+++ android/app/src/main/java/io/unthrottled/sogos/tacmod/alarm/AlarmReceiver.kt
@@ -0,0 +1,11 @@
+package io.unthrottled.sogos.tacmod.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        println("Ahh shit, here we go.")
+    }
+}
