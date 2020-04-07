b40a96841e6f090ed809dbd794aa9490d83f6bf5
DATBOI <alexsimons9999@gmail.com>
2020-04-07 18:23:54 -0500

    Working failing test

---
+++ android/app/src/test/java/io/unthrottled/sogos/tacmod/pomodoro/PomodoroModuleTest.kt
@@ -0,0 +1,13 @@
+package io.unthrottled.sogos.tacmod.pomodoro
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PomodoroModuleTest {
+
+  @Test
+  fun stopItGetSomeHelp() {
+    assertTrue(false)
+  }
+}
